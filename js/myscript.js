bd22762710a538acc577c1e764498af01154c216
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-16 13:58:03 +0200

    fizz buzz con i colori

--- js/myscript.js
+++ js/myscript.js
@@ -3,23 +3,26 @@ const ul = document.querySelector("ul");
 
 for(let i=0; i <= 100; i++){
 
-    if( ((i % 5) == 0) && ((i % 2) == 0)){
+    if( ((i % 5) == 0) && ((i % 3) == 0)){
         console.log("FizzBuzz : " + i);
         const li = document.createElement("li");
+        li.classList.add("rosso");
         li.append("FIZZBUZZ " + i);
         ul.appendChild(li);
     }
     else if((i % 5) == 0){{
         console.log("Buzz: " + i);
         const li = document.createElement("li");
+        li.classList.add("verde");
         li.append("BUZZ " + i);
         ul.appendChild(li);
 
         }
     }
-    else if((i % 2) == 0){
+    else if((i % 3) == 0){
         console.log("Fizz: " + i);
         const li = document.createElement("li");
+        li.classList.add("giallo");
         li.append("FIZZ " + i);
         ul.appendChild(li);
     }
