17ef40dcf2090f8b2cec57400bc8433266ec8dde
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-16 13:49:55 +0200

    numeri divisibili per 5 e 2

--- js/myscript.js
+++ js/myscript.js
@@ -2,23 +2,35 @@
 const ul = document.querySelector("ul");
 
 for(let i=0; i <= 100; i++){
-    console.log(i);
 
     if( ((i % 5) == 0) && ((i % 2) == 0)){
-        console.log("Sono multiplo di 5 e di 2 : " + i)
+        console.log("FizzBuzz : " + i);
+        const li = document.createElement("li");
+        li.append("FIZZBUZZ " + i);
+        ul.appendChild(li);
     }
     else if((i % 5) == 0){{
-        console.log("Sono multiplo di 5: " + i)
+        console.log("Buzz: " + i);
+        const li = document.createElement("li");
+        li.append("BUZZ " + i);
+        ul.appendChild(li);
+
         }
     }
     else if((i % 2) == 0){
-        console.log("Sono multiplo di 2: " + i);
+        console.log("Fizz: " + i);
+        const li = document.createElement("li");
+        li.append("FIZZ " + i);
+        ul.appendChild(li);
+    }
+    else{
+        console.log(i);
+        const li = document.createElement("li");
+        li.append(i);
+        ul.appendChild(li);
     }
 
-    const li = document.createElement("li");
-    li.append("numero : " + i);
-
-    ul.appendChild(li);
+    
 }
 
 
