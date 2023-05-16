0fab5cf9497b4904ba4de062422c195ca42c8f64
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-16 15:46:57 +0200

    numeri da 1 a 100

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,7 @@
 
 const ul = document.querySelector("ul");
 
-for(let i=0; i <= 100; i++){
+for(let i=1; i < 101; i++){
 
     if( ((i % 5) == 0) && ((i % 3) == 0)){
         console.log("FizzBuzz : " + i);
